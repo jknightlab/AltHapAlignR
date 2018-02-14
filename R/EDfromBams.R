be8130b3b85b1335be70aaf95db2b8bae359d746
Wanseon Lee <wl@well.ox.ac.uk>
2018-02-14 03:57:05 +0000

    modi

--- R/EDfromBams.R
+++ R/EDfromBams.R
@@ -4,18 +4,21 @@
 #' @param type selecting gene feature
 #' @export
 #' @import rtracklayer
+#' @importFrom rtracklayer blocks
+#' @importFrom rtracklayer path
 #' @examples
+#' gtf=system.file("extdata", "gencode.v21.chr_patch_hapl_HLA.annotation.gtf", 
+#'                  package = "AltHapAlignR")
 #' output <- getGeneList(gtf, type="protein_coding")
 #' 
 #' # Output data
 #' # summary of gene information, format: data.frame
 
-gtf="inst/extdata/gencode.v21.chr_patch_hapl_HLA.annotation.gtf"
 
 getGeneList <- function(gtf, type="protein_coding"){
         g <- import(gtf, "gtf")
         g <- subset(g, type=="gene")
-        g <- data.frame(chr=seqnames(g),
+        g <- data.frame(chr=g@seqnames,
                         start=g@ranges@start,
                         end=g@ranges@start + g@ranges@width,
                         gene_name=g$gene_name,
@@ -33,13 +36,17 @@ getGeneList <- function(gtf, type="protein_coding"){
 #' @param virtualenv path of virtualenv, optional
 #' @export
 #' @examples
-#' EDfromBams(bamFiles="./inst/extdata/mapping2*bam", gtf="./inst/extdata/gencode.v25.chr_patch_hapl_HLA.annotation.gtf", output_name="output.txt", virtualenv=NA, r="VARSL-VARS2,C6orf205-MUC21")
-
+#' bamFiles=system.file("extdata", "mapping2*bam", package = "AltHapAlignR")
+#' gtf=system.file("extdata", "gencode.v21.chr_patch_hapl_HLA.annotation.gtf", 
+#'                  package = "AltHapAlignR")
+#' EDfromBams(bamFiles, gtf, output_name="output.txt", virtualenv=0, r="VARSL-VARS2,C6orf205-MUC21")
 
           
-EDfromBams <- function(bamFiles, gtf, output_name="ed_table.txt", virtualenv=NA, r="VARSL-VARS2,C6orf205-MUC21"){
+EDfromBams <- function(bamFiles, gtf, output_name="ed_table.txt", virtualenv=0, r="VARSL-VARS2,C6orf205-MUC21"){
+    
+    py_path=system.file("scripts", "make_a_table.py", package = "AltHapAlignR")
 
-    py_path=paste0(system.file(package="AltHapAlignR"), "./inst/scripts/make_a_table.py")
+    #py_path="./inst/scripts/make_a_table.py"
     bamFiles <- paste(bamFiles, collapse = " ")
     
     command <- paste("python", py_path,  gtf, bamFiles, ">", output_name )
@@ -53,12 +60,12 @@ EDfromBams <- function(bamFiles, gtf, output_name="ed_table.txt", virtualenv=NA,
     }
     
     
-    if(!is.na(virtualenv) ){
+    if( virtualenv!=0 ){
           command <- paste0("./inst/scripts/wrapper.sh ", virtualenv, " ", command )
-          print(command)
+          #print(command)
     }
-    if(is.na(virtualenv) ){
-      print(command)
+    if( virtualenv==0 ){
+          #print(command)
     }
     
     output= system(command, intern=T)
@@ -66,28 +73,5 @@ EDfromBams <- function(bamFiles, gtf, output_name="ed_table.txt", virtualenv=NA,
 }
 
 
-# ./make_a_table.py -r VARSL VARS2 -r C6orf205 MUC21 gencode.v21.only_MHC.annotation.gtf */*picard*bam > output_file
-# example: 
-gtf="./inst/extdata/gencode.v25.chr_patch_hapl_HLA.annotation.gtf"
-#bamFiles="mapping2apd.bam,mapping2apd.bam,mapping2cox.bam,mapping2mann.bam,mapping2mcf.bam,mapping2qbl.bam,mapping2ssto.bam,mapping2pgf.bam"
-bamFiles="./inst/extdata/mapping2*"
-output_name ="test.txt"
-#export PYTHONPATH=/well/jknight/Irina/Programs/HTSeq-0.6.1/build/lib.linux-x86_64-2.7:/well/jknight/software/rescomp/lib/python2.7/site-packages/${PYTHONPATH:+:$PYTHONPATH}
-#export PYTHONPATH=/Users/wanlee/Documents/well/test_althap:/Library/Python/2.7/site-packages/${PYTHONPATH:+:$PYTHONPATH}
 
 
-#EDfromBams(bamFiles, gtf, output_name="with_virtual.txt", virtualenv="/Users/wanlee/Documents/well/test_althap/althapalign_virtualenv", r="VARSL-VARS2,C6orf205-MUC21")
-
-
-#EDfromBams(bamFiles, gtf, output_name="without_virtual.txt", r="VARSL-VARS2,C6orf205-MUC21")
-
-
-#EDfromBams(bamFiles, gtf, output_name="test_with_virtual.txt", virtualenv=1, r="VARSL-VARS2,C6orf205-MUC21")
-
-EDfromBams(bamFiles, gtf, output_name="without_virtual_21.txt", r="VARSL-VARS2,C6orf205-MUC21")
-EDfromBams(bamFiles, gtf, output_name="without_virtual_25_r2_1.txt", r="VARSL-VARS2,C6orf205-MUC21")
-
-
-gtf="./inst/extdata/gencode.v21.chr_patch_hapl_HLA.annotation.gtf"
-bamFiles="./inst/extdata/mapping2*"
-EDfromBams(bamFiles, gtf, output_name="without_virtual_v21_r1_1.txt", r="VARSL-VARS2,C6orf205-MUC21")
